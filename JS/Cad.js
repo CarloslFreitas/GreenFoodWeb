340a9b687a8e6d283c1a16ad86c2902d668d591f
carlos <carlos.d.freitas0@hotmail.com>
2022-04-03 12:04:26 -0300

    Em desenvolvimento

---
+++ JS/Cad.js
@@ -0,0 +1,130 @@
+let btn = document.querySelector('#verSenha')
+let btnConfirmSenha = document.querySelector('#verConfirmSenha')
+
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
+let validNome = false
+
+let usuario = document.querySelector('#usuario')
+let labelUsuario = document.querySelector('#labelUsuario')
+let validUsuario = false
+
+let email = document.querySelector('#email')
+let labelEmail = document.querySelector('#labelEmail')
+let validEmail = false
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+let validSenha = false
+
+let confirmSenha = document.querySelector('#confirmSenha')
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
+let validConfirmSenha = false
+
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
+
+nome.addEventListener('keyup', () => {
+    if (nome.value.length <= 2) {
+        labelNome.setAttribute('style', 'color: red')
+        labelNome.innerHTML = 'Nome *Insira no minimo 3 caracteres'
+        nome.setAttribute('style', 'border-color: red')
+        validNome = false
+    } else {
+        labelNome.setAttribute('style', 'color: green')
+        labelNome.innerHTML = 'Nome'
+        nome.setAttribute('style', 'border-color: green')
+        validNome = true
+    }
+})
+
+usuario.addEventListener('keyup', () => {
+    if (usuario.value.length <= 4) {
+        labelUsuario.setAttribute('style', 'color: red')
+        labelUsuario.innerHTML = 'Usuario *Insira no minimo 5 caracteres'
+        usuario.setAttribute('style', 'border-color: red')
+        validUsuario = false
+    } else {
+        labelUsuario.setAttribute('style', 'color: green')
+        labelUsuario.innerHTML = 'Usuario'
+        usuario.setAttribute('style', 'border-color: green')
+        validUsuario = true
+    }
+})
+
+email.addEventListener('keyup', () => {
+    if (email.value.length <= 8) {
+        labelEmail.setAttribute('style', 'color: red')
+        labelEmail.innerHTML = 'Email *Insira um email valido'
+        email.setAttribute('style', 'border-color: red')
+        validEmail = false
+    } else {
+        labelEmail.setAttribute('style', 'color: green')
+        labelEmail.innerHTML = '<strong>Email</strong>'
+        email.setAttribute('style', 'border-color: green')
+        validEmail = true
+    }
+})
+
+senha.addEventListener('keyup', () => {
+    if (senha.value.length <= 7) {
+        labelSenha.setAttribute('style', 'color: red')
+        labelSenha.innerHTML = 'Senha *Insira no minimo 8 caracteres'
+        senha.setAttribute('style', 'border-color: red')
+        validSenha = false
+    } else {
+        labelSenha.setAttribute('style', 'color: green')
+        labelSenha.innerHTML = '<strong>Senha</strong>'
+        senha.setAttribute('style', 'border-color: green')
+        validSenha = true
+    }
+})
+
+confirmSenha.addEventListener('keyup', () => {
+    if (senha.value != confirmSenha.value) {
+        labelConfirmSenha.setAttribute('style', 'color: red')
+        labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
+        confirmSenha.setAttribute('style', 'border-color: red')
+        validConfirmSenha = false
+    } else {
+        labelConfirmSenha.setAttribute('style', 'color: green')
+        labelConfirmSenha.innerHTML = 'Confirmar Senha'
+        confirmSenha.setAttribute('style', 'border-color: green')
+        validConfirmSenha = true
+    }
+})
+
+function cadastrar() {
+    if (validNome && validUsuario && validEmail && validSenha && validConfirmSenha) {
+        msgSuccess.setAttribute('style', 'display: block')
+        msgSuccess.innerHTML = '<strong>Cadastro Realizado!</strong>'
+        msgError.setAttribute('style', 'display: none')
+        msgError.innerHTML = ''
+    } else {
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = '<strong>Preencha os campos corretamente.</strong>'
+        msgSuccess.innerHTML = ''
+        msgSuccess.setAttribute('style', 'display: none')
+    }
+}
+
+btn.addEventListener('click', () => {
+    let inputSenha = document.querySelector('#senha')
+
+    if (inputSenha.getAttribute('type') == 'password') {
+        inputSenha.setAttribute('type', 'text')
+    } else {
+        inputSenha.setAttribute('type', 'password')
+    }
+})
+
+
+btnConfirmSenha.addEventListener('click', () => {
+    let inputConfirmSenha = document.querySelector('#confirmSenha')
+
+    if (inputConfirmSenha.getAttribute('type') == 'password') {
+        inputConfirmSenha.setAttribute('type', 'text')
+    } else {
+        inputConfirmSenha.setAttribute('type', 'password')
+    }
+})
